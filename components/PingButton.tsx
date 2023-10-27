066843cd9b28a9a7ce628f6d285ce45f27627ea9
ra2dd <radoslawpryw@gmail.com>
2023-10-27 18:12:03 +0200

    add ping button functionality

--- components/PingButton.tsx
+++ components/PingButton.tsx
@@ -1,16 +1,68 @@
 import { FC, useState } from 'react'
 import styles from '../styles/PingButton.module.css'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+import * as web3 from '@solana/web3.js'
 
 export const PingButton: FC = () => {
-
+	const { connection } = useConnection();
+	const { publicKey, sendTransaction } = useWallet();
+	const [signature, setSignature] = useState('');
+	
     const onClick = () => {
-        console.log('Ping!')
-    }
+        if (!connection || !publicKey) {
+			return;
+		}
+
+		const programId = new web3.PublicKey('ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa');
+		const programDataAccount = new web3.PublicKey('Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod');
+		const transaction = new web3.Transaction();
+
+		const instruction = new web3.TransactionInstruction({
+			keys: [
+				{
+					pubkey: programDataAccount,
+					isSigner: false,
+					isWritable: true,
+				},
+			],
+			programId,
+		});
+
+		transaction.add(instruction);
+		sendTransaction(transaction, connection).then((sig) => {
+			console.log(sig);
+			setSignature(sig);
+		});
+    };
+
+	function sigLink() {
+		if (signature == '') {
+			return;
+		}
+
+		const link = `https://explorer.solana.com/tx/${signature}?cluster=devnet`;
+		return (
+			<div>
+				<p>
+					Check transaction <a href={link}>on sol explorer</a>
+				</p>
+			</div>
+		)				
+	}
     
 	return (
-		<div className={styles.buttonContainer} onClick={onClick}>
-			<button className={styles.button}>Ping!</button>
+		<div className={styles.buttonContainer}>
+			{publicKey ? (
+				<>
+					<button className={styles.button} onClick={onClick}>Ping!</button>
+				</>
+			) : (
+				<>
+					<p>Connect sol wallet to ping account.</p>
+				</>
+			)}
+			{sigLink()}
 		</div>
-	)
+	);
 }
 
